a73a2ece2758d228ca8e66fbdb2e082964e0dae6
hmsvigle <hmsvigle@gmail.com>
2021-03-28 17:27:43 +0530

    Add files via upload

---
+++ AZ-303/Day01/Powershell script to create resource group.ps1
@@ -0,0 +1,19 @@
+﻿
+
+Install-Module -Name Az -Scope CurrentUser
+
+
+#Running Powershell is disabled on this system
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+
+Import-Module -Name Az
+
+Login-AzAccount
+
+Select-AzSubscription -SubscriptionId d1be3007-4f5f-4696-87c0-d55ea40f1fc8
+
+New-AzResourceGroup -Name rg2 -Location 'East US'
+
+New-AzResourceGroup -Name rg3

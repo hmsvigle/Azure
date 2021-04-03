8081ac694bcb592d5d725a7969866d45f1772572
hmsvigle <hmsvigle@gmail.com>
2021-04-03 12:48:05 +0530

    Update Powershell script to create resource group.ps1

--- AZ-303/Day01/Powershell script to create resource group.ps1
+++ AZ-303/Day01/Powershell script to create resource group.ps1
@@ -17,3 +17,10 @@ Select-AzSubscription -SubscriptionId d1be3007-4f5f-4696-87c0-d55ea40f1fc8
 New-AzResourceGroup -Name rg2 -Location 'East US'
 
 New-AzResourceGroup -Name rg3
+
+--------------------
+# Create New RG:
+New-AzResourceGroup -Name hp-az104-03a-rg1 -Location 'East US'
+# Verify RG 
+Get-AzResourceGroup -Name hp-*
+
